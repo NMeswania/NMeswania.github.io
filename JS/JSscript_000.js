520950597f55ac03c5e3e5b7e7c7298fdd88bc9b
NMeswania <n.meswania@gmail.com>
2018-08-06 19:01:06 +0100

    Initial Commit V18
    Project main and Software pages responsive CSS all done

--- JS/JSscript_000.js
+++ JS/JSscript_000.js
@@ -6,6 +6,18 @@ function myFunction(compInfoNum) {
 	var buts = document.getElementsByClassName("compImageButton");	// all competancy image buttons
 	var x = document.getElementById("compInfo" + compInfoNum);		// desired div to toggle
 	var showing = false;
+	var heightData = [
+		{
+			_height: [19, 20, 18, 16, 15, 15, 13, 13],
+			_step: 1,
+			_unit: "vw"
+		},
+		{
+			_height: [550, 450, 450, 350, 350, 325, 275, 300],
+			_step: 25,
+			_unit: "px"
+		}
+	];
 	
 	// reduce all panel heights, check if the selected panel is already showing
 	var i;
@@ -20,22 +32,29 @@ function myFunction(compInfoNum) {
 	// open selected panel
 	if (!showing) {
 		x.style.display = "block";
-		increaseHeight(compInfoNum);
+		increaseHeight(compInfoNum, heightData);
 	}
 }
 
 // increase panel height animation function
-function increaseHeight(_number) {
+function increaseHeight(_number, _heightData) {
 	var _x = document.getElementById("compInfo" + _number);
-	var _height = 18;
+	var _screen;
+	if (window.innerWidth >= 601) {
+		_screen = 0;
+	} else {
+		_screen = 1;
+	}
+	
+	var _height = _heightData[_screen]._height[_number];
 	var _currentHeight = 0;
 	var _id = setInterval(frame, 25);
 	function frame() {
 		if (_currentHeight > _height) {
 			clearInterval(_id);
 		} else {
-			_currentHeight = _currentHeight + 1;
-			_x.style.height = _currentHeight + "vw";
+			_currentHeight = _currentHeight + _heightData[_screen]._step;
+			_x.style.height = _currentHeight + _heightData[_screen]._unit;
 		}
 	}
 }
